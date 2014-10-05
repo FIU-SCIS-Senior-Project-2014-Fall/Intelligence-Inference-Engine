e541d02e73b0e64c059ed4e95efd42b628792206
jacos046 <jacos046@fiu.edu>
2014-10-04 20:06:27 -0400

    Updated scripts and search form
    Modified some things in the search form for it to work properly with the
    back end script. Finished the back end script to generate the basic
    starter query that will drive the search form.

--- localweb/scripts/searchscript.php
+++ localweb/scripts/searchscript.php
@@ -1,7 +1,36 @@
 <?php
 
-echo var_dump($_POST) . "<br>";
+//echo var_dump($_POST) . "<br>";
 
+
+
+$query = "PREFIX iie: &lt/fake/iie/types&gt";
+
+foreach (explode(",",$_POST["prefixName"]) as $prefix);
+	$query = $query . "PREFIX " . $prefix;
+
+$query = str_replace("<","&lt",$query);
+$query = str_replace(">","&gt",$query);
+
+$query = $query . "SELECT ?subject WHERE { ";
+
+$size = intval($_POST['size']);
+
+for($i=1; $i<=$size; $i++)
+{
+	if($i == 1)
+		$query = $query . "?subject ";
+	$query = $query . "iie:" . $_POST['data-pred'. strval($i)] . " ";
+	$query = $query . "\"" . $_POST['data-'.strval($i)] ."\"" ;
+	if($i < $size)
+		$query = $query . "; " ;
+	else
+		$query = $query . ". " ;
+}
+
+$query = $query . " }";
+
+print $query;
 
 
 
