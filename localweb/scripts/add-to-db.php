eaf237242dfbadb22b2f5796f2d45d17218b8df4
Lazaro Herrera <lazherrera@gmail.com>
2014-10-18 20:01:26 -0400

    Custom Search Query Storage/Retrieval

--- localweb/scripts/add-to-db.php
+++ localweb/scripts/add-to-db.php
@@ -1,13 +1,20 @@
 <?php
-	$con=mysqli_connect("127.0.0.1","root","","queries");
-
-	if (mysqli_connect_errno()) {
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
-	else
+	function addToDB($query, $tags)
 	{
-		echo "Everything good";
-	}
+		$con=mysqli_connect("127.0.0.1","root","","queries");
+
+		if (mysqli_connect_errno()) {
+			return "Store unsuccessful ". mysqli_connect_error();
+		}
+		else
+		{
+			$result = mysqli_query($con,"INSERT INTO stored_queries (query, tags, submission_time) VALUES ('$query', '$tags', now())");
+			if (mysqli_connect_errno()) {
+				return "Store unsuccessful ". mysqli_connect_error();
+			}
+		}
 	
-	mysqli_close($con);
+		mysqli_close($con);
+	}
+	echo addToDB(trim($_REQUEST['query']),trim($_REQUEST['tags']));
 ?>
--- localweb/scripts/search-db.php
+++ localweb/scripts/search-db.php
@@ -1,13 +1,40 @@
 <?php
-	$con=mysqli_connect("127.0.0.1","root","","queries");
-
-	if (mysqli_connect_errno()) {
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
-	else
+	function searchDB($tags)
 	{
-		echo "Everything good";
-	}
+		$con=mysqli_connect("127.0.0.1","root","","queries");
+
+		if (mysqli_connect_errno()) {
+			return "Read unsuccessful ". mysqli_connect_error();
+		}
+		else
+		{
+			$query = "SELECT query, tags, submission_time FROM stored_queries WHERE";
+			$splitTags = explode(",",$tags);
+			foreach($splitTags as $tag) 
+			{
+				$query .= " tags LIKE '$tag%' OR";
+			}
+			$query = substr($query, 0, -3);
+			
+			$return = "";
+			if($result = mysqli_query($con, $query))
+			{
+				while ($row=mysqli_fetch_row($result))
+				{
+					$return .= "//Tags: " . $row[1] . "\n";
+					$return .= "//Submission Time: " . $row[2] . "\n";
+					$return .= "" . $row[0] . "\n\n";
+				}
+				mysqli_free_result($result);
+			}
+			
+			if (mysqli_connect_errno()) {
+				return "Read unsuccessful ". mysqli_connect_error();
+			}
+			return $return;
+		}
 	
-	mysqli_close($con);
+		mysqli_close($con);
+	}
+	echo searchDB(trim($_REQUEST['tags']));
 ?>
