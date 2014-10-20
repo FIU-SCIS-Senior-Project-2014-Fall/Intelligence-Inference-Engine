c44b6e00366f6d43f42c71e57e439e67982439cc
Lazaro Herrera <lazherrera@gmail.com>
2014-10-20 16:50:04 -0400

    Fix to custom queries.

--- localweb/scripts/custom_query.php
+++ localweb/scripts/custom_query.php
@@ -3,13 +3,10 @@
 
 include 'httpful.phar';
 
-$queryURL = "http://iie-dev.cs.fiu.edu:3030/iie/query";
-$queryContents = $_POST['query'];
+$queryURL = "http://iie-dev.cs.fiu.edu:3030/iie/query?query=" . urlencode($_POST['query']);
 echo "Query URL: " . $queryURL;
-echo "<br>Query Contents: " . $queryContents;
 
 $response = \Httpful\Request::get($queryURL)
-			->param("query", $queryContents)
 			->send();
 
 echo "<br>Response: " . $response;
