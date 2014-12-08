49bb3674796108ebd96cda52bb20cd09559b2c6e
Lazaro Herrera <lherr012@fiu.edu>
2014-12-07 19:26:13 -0500

    Some fixes.
    Some fixes to custom search, search and submit.

--- localweb/scripts/custom_query.php
+++ localweb/scripts/custom_query.php
@@ -3,11 +3,10 @@
 
 include 'httpful.phar';
 
-$queryURL = "http://iie-dev.cs.fiu.edu:3030/iie/query?query=" . urlencode($_POST['query']);
-echo "Query URL: " . $queryURL;
+$queryURL = "http://iie-dev.cs.fiu.edu:3030/iie/query?query=" . urlencode($_POST['query']) . "&output=xml&stylesheet=";		
 
 $response = \Httpful\Request::get($queryURL)
 			->send();
 
-echo "<br>Response: " . $response;
+echo $response;
 ?>
